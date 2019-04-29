502cc0839a4460c0b701bc1f71d49e9e467eb9ba
Sergio Capozzi <sergiocapozzi@outlook.com>
2019-04-29 19:55:19 +0100

    Added EnableMouseInput

--- WpfTestContextMenu/App.xaml.cs
+++ WpfTestContextMenu/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using LibVLCSharp.Shared;
 
 namespace WpfTestContextMenu
 {
@@ -13,5 +14,9 @@ namespace WpfTestContextMenu
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Core.Initialize();
+        }
     }
 }
--- WpfTestContextMenu/MainWindow.xaml.cs
+++ WpfTestContextMenu/MainWindow.xaml.cs
@@ -1,28 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using System.Windows.Forms;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WinForms;
 
 namespace WpfTestContextMenu
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    ///     Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly LibVLC libVLC;
+
+        private VideoView WinFormVideoView;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            libVLC = new LibVLC();
+
+            // Winform
+            var media = new Media(libVLC, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", FromType.FromLocation);
+            WinFormVideoView = new VideoView();
+            var mp = new MediaPlayer(media);
+            WindowsFormsHost.Child = WinFormVideoView;
+            mp.EnableMouseInput = true;
+            WinFormVideoView.MouseClick += WinFormVideoView_MouseClick;
+            WinFormVideoView.MediaPlayer = mp;
+
+            //WPF
+            var media1 = new Media(libVLC, "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", FromType.FromLocation);
+            var mp1 = new MediaPlayer(media1);
+            mp1.EnableMouseInput = true;
+            WpfVideoView.MediaPlayer = mp1;
         }
+
+        private void WinFormVideoView_MouseClick(object sender, MouseEventArgs e)
+        {
+            WindowsFormsHost.ContextMenu.IsOpen = true;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            WpfVideoView.MediaPlayer.Play();
+            WinFormVideoView.MediaPlayer.Play();
+        }
+
     }
 }
